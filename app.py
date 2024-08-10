02f55880676ba2ec4d6c11f477ad4aff4a1c8e81
GUAN HONG LAI <101321011+tarucguanhong@users.noreply.github.com>
2024-08-10 16:07:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import ast
 import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
+from openai.embeddings_utils import cosine_similarity
 import openai
 
 openai.api_key =  st.secrets["mykey"]
@@ -19,24 +19,24 @@ def get_embedding(text, model="text-embedding-ada-002"):
 
 # Function to find the best answer
 def find_best_answer(user_question):
-    # Get embedding for the user's question
-    user_question_embedding = np.array(get_embedding(user_question))
-    
-    # Calculate cosine similarities for all questions in the dataset
-    similarities = df['Question_Embedding'].apply(lambda x: cosine_similarity([x], [user_question_embedding])[0][0])
+   # Get embedding for the user's question
+   user_question_embedding = get_embedding(user_question)
 
-    # Find the most similar question and get its corresponding answer
-    most_similar_index = similarities.idxmax()
-    max_similarity = similarities.max()
+   # Calculate cosine similarities for all questions in the dataset
+   df['Similarity'] = df['Question_Embedding'].apply(lambda x: cosine_similarity(x, user_question_embedding))
 
-    # Set a similarity threshold to determine if a question is relevant enough
-    similarity_threshold = 0.6  # Adjust this value as needed
+   # Find the most similar question and get its corresponding answer
+   most_similar_index = df['Similarity'].idxmax()
+   max_similarity = df['Similarity'].max()
 
-    if max_similarity >= similarity_threshold:
-        best_answer = df.loc[most_similar_index, 'Answer']
-        return best_answer, max_similarity
-    else:
-        return "I apologize, but I don't have information on that topic yet. Could you please ask other questions?", max_similarity
+   # Set a similarity threshold to determine if a question is relevant enough
+   similarity_threshold = 0.6  # You can adjust this value
+
+   if max_similarity >= similarity_threshold:
+      best_answer = df.loc[most_similar_index, 'Answer']
+      return best_answer
+   else:
+      return "I apologize, but I don't have information on that topic yet. Could you please ask other questions?"
 
 # Streamlit interface
 st.title("Health FAQ Assistant")
@@ -47,9 +47,8 @@ user_question = st.text_input("Enter your question:")
 # Button to search for an answer
 if st.button('Find Answer'):
     if user_question:
-        answer, similarity_score = find_best_answer(user_question)
+        answer = find_best_answer(user_question)
         st.write(f"Answer: {answer}")
-        st.write(f"Similarity Score: {similarity_score:.2f}")
     else:
         st.write("Please enter a question.")
 
